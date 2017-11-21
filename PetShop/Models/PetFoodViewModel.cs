e4f44ea760cd61a24ce0d95e1f88cd150f63f0b2
kevingilfether <kevingilfether@gmail.com>
2017-11-21 12:01:01 -0500

    Created view model for PetFood

---
+++ PetShop/Models/PetFoodViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetShop.Models
+{
+    public class PetFoodViewModel
+    {
+        public Pet pet { get; set; }
+        public Food food { get; set; }
+
+    }
+}
